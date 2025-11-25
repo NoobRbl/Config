8cde257af3f8c3d4251b891bc219682523235c09
NoobRbl <goodscripts001@gmail.com>
2025-11-25 21:46:24 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,41 @@
+window.scriptData = [
+  {
+    id: "gag_script",
+    title: "coming soon [GAG]",
+    url: "coming soon",
+    description: "Script Spawner, Auto For Grow A Garden",
+    category: "Spawner",
+    tags: ["helper", "gag", "roblox", "spawner"],
+    image: "https://tr.rbxcdn.com/180DAY-91f8ec73480a974786806c66d92dd30e/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "sab_script",
+    title: "NOOBHUB [SAB]",
+    url: "https://raw.githubusercontent.com/NoobRoblox/StealABrainrot/refs/heads/main/TongHopNoobRoblox",
+    description: "Script TongHop, EXECUTE ALL EVENT, SPAWNER, DUPLICATE For Steal A Brainrot",
+    category: "Helper",
+    tags: ["sab", "allinone", "Spawner", "Duplicate"],
+    image: "https://tr.rbxcdn.com/180DAY-b6ad9f8a2edddee0365eed4f19a8caf9/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "99_script",
+    title: "coming soon [99NITF]",
+    url: "coming soon",
+    description: "Script Mob ESP, Auto Farm Days, Diamonds, Bring For 99 Nights In The Forest",
+    category: "Survival",
+    tags: ["forest", "night", "survival", "esp"],
+    image: "https://tr.rbxcdn.com/180DAY-643b9b9285f3bd9b63cf392db12a4d87/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "pvb_script",
+    title: "coming soon [PVB]",
+    url: "coming soon",
+    description: "Script Spawner For Plants Vs Brainrots",
+    category: "Spawner",
+    tags: ["pvb", "plants", "brainrot", "spawner"],
+    image: "https://tr.rbxcdn.com/180DAY-a933a9ba765efea4061f4a101f18edd2/768/432/Image/Webp/noFilter"
+  }
+];
